e0e22ac9130465ad29290cd9cb3cad4343a60096
billmcculloch <billrmcculloch@gmail.com>
2023-03-21 12:31:15 +1300

    set up profile pic and about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,32 @@ import { useEffect, useRef, useState } from "react";
 import s from "./About.module.scss";
 
 function About() {
-  return <div className={s.wrapper} id={"about"}></div>;
+  return (
+    <div className={s.wrapper} id={"about"}>
+      <div className={s.textarea}>
+        <p>
+          Welcome to my portfolio! I’ve been lucky enough to work on some
+          exciting projects from start to finish as a Junior Developer at Roady,
+          including working in a team of two to build and release the Roady App,
+          customer web portal and API. This has given me exposure to modern
+          technologies including Flutter, React, Typescript, GraphQL, Node.js to
+          name a few. 
+        </p>
+        <p>
+          I’m always looking to learn and grow as a developer, and with a
+          background in marketing it’s not surprising that I’ve found a passion
+          for building beautiful user experiences for both mobile and the web. 
+        </p>
+        <p>
+          I’m available for work in London from July 2023, so get in touch and
+          let’s make something awesome together.
+        </p>
+      </div>
+      <div className={s.profilepic}>
+        <div className={s.img} />
+      </div>
+    </div>
+  );
 }
 
 export default About;
