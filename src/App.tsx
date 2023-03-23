12669d50fb04e64be67d4a0abb14e86de1cbfb01
billmcculloch <billrmcculloch@gmail.com>
2023-03-24 09:09:22 +1300

    set up and turn off loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Loader from "./components/Loader";
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
 
   return (
     <>
