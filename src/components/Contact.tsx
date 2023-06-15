5f64a0673d924421e9ba67fde6ac50177502341f
billmcculloch <billrmcculloch@gmail.com>
2023-06-15 12:15:02 +0200

    _blank to links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
       <div className={s.container}>
         <div className={s.email}>
           <p style={{ textDecoration: "underline" }}>Email</p>
-          <a href={"mailto: billrmcculloch@gmail.com"}>
+          <a href={"mailto: billrmcculloch@gmail.com"} target={"_blank"}>
             <p>
               billrmcculloch@gmail.com &nbsp;&nbsp;
               <img src="/src/assets/arrow.svg" />
@@ -18,7 +18,10 @@ export default function Contact() {
         </div>
         <div className={s.linkedIn}>
           <p style={{ textDecoration: "underline" }}>LinkedIn</p>
-          <a href={"https://www.linkedin.com/in/bill-mcculloch/"}>
+          <a
+            href={"https://www.linkedin.com/in/bill-mcculloch/"}
+            target={"_blank"}
+          >
             <p>
               linkedin.com/in/bill-mcculloch &nbsp;&nbsp;
               <img src="/src/assets/arrow.svg" />
@@ -27,7 +30,7 @@ export default function Contact() {
         </div>
         <div className={s.github}>
           <p style={{ textDecoration: "underline" }}>GitHub</p>
-          <a href={"https://github.com/billmcculloch"}>
+          <a href={"https://github.com/billmcculloch"} target={"_blank"}>
             <p>
               github.com/billmcculloch &nbsp;&nbsp;
               <img src="/src/assets/arrow.svg" />
